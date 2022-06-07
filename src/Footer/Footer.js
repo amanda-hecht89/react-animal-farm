b2fcacbca137d6425d2561d27f741563ebf58d63
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-07 15:59:49 -0600

    css

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  return <footer>Animal Farm By Amanda</footer>;
+  return <footer>amandahecht89@gmail.com</footer>;
 }
